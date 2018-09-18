2b3e5988f7ef89b900b9e73b29c7a4f607bd77d7
phillip <phillipt@bu.edu>
2018-09-18 16:03:28 -0400

    Added saving data to a txt file.

--- Code/car_detection.py
+++ Code/car_detection.py
@@ -16,8 +16,8 @@ import cv2
 import numpy as np
 
 #create VideoCapture object and read from video file
-cap = cv2.VideoCapture('cars.mp4')
-#cap = cv2.VideoCapture('cars_%03d.jpg')
+#cap = cv2.VideoCapture('cars.mp4')
+cap = cv2.VideoCapture('cars_%03d.jpg')
 #cap = cv2.VideoCapture('cars_480.mp4')
 #cap = cv2.VideoCapture('cars_360.mp4')
 
@@ -27,6 +27,8 @@ cap = cv2.VideoCapture('cars.mp4')
 #use trained cars XML classifiers
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
+file = open('output.txt', 'w')
+
 #read until video is completed
 while True:
     #reset car count to 0 for each frame
@@ -34,8 +36,13 @@ while True:
 
     #capture frame by frame
     ret, frame = cap.read()
+
     #convert video into gray scale of each frames
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        pass
+    except cv2.error:
+        break
 
     #detect cars in the video
     cars = car_cascade.detectMultiScale(gray, 1.1, 3)
@@ -50,6 +57,8 @@ while True:
     cv2.imshow('video', frame)
     #out.write(frame) # writes to output file
     print "{}".format(car_count)
+    file.write("{}\n".format(car_count))
+
 
     #press q on keyboard to exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -60,3 +69,4 @@ cap.release()
 #out.release()
 #close all the frames
 cv2.destroyAllWindows()
+file.close()
